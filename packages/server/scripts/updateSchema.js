eaa3d4028955afec43324d95813d57f050f8cfac
Pedro Paulo Bazzo <pedropbazzo@outlook.com>
2021-03-03 06:01:31 -0300

    ---

--- packages/server/scripts/updateSchema.js
+++ packages/server/scripts/updateSchema.js
@@ -10,7 +10,7 @@
  * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-
+ 
 import fs from 'fs';
 import path from 'path';
 import { schema } from '../src/schema';
